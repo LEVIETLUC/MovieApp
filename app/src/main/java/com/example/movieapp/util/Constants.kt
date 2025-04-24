6ddfca50dadb478b53c8f996bddf83f202a5b388
Nguyen Minh Triet <minhtriet.nguyen.int@shopback.com>
2025-04-25 03:30:21 +0700

    Hide api key

--- app/src/main/java/com/example/movieapp/util/Constants.kt
+++ app/src/main/java/com/example/movieapp/util/Constants.kt
@@ -1,12 +1,12 @@
 package com.example.movieapp.util
 
+import com.example.movieapp.BuildConfig
+
 object Constants {
     const val TMDB_BASE_URL = "https://api.themoviedb.org/3/"
     const val TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w342"
 
-    // Replace with your API key
-    const val TMDB_API_KEY = "a57d71add0e08f164dad06ed4db1a291"
-//    const val TMDB_API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNTdkNzFhZGQwZTA4ZjE2NGRhZDA2ZWQ0ZGIxYTI5MSIsIm5iZiI6MTc0NTE1Mjc1My41NTAwMDAyLCJzdWIiOiI2ODA0ZWFmMWFjMDJkNDQwN2JhYThjMWIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.JjuCtOe5ROMaj3IUD0tWdeaXbDOt5iS5yb3esYBOmKY"
 
+   val TMDB_API_KEY = BuildConfig.TMDB_API_KEY
 
 }
